f7dca8ff25733758a43aae0428630934fbf6a516
Sai Raghava Ithireddy <131619104+raghava4327@users.noreply.github.com>
2024-02-06 02:24:42 +0530

    image adjusting

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(){
     <h1>Weather Dashboard</h1>
     <input type="text" placeholder="Enter location or pincode"/>
     <div className="navbar-icons">
-    <img src="../weather-dashboard/src/assets/Bell.svg" />
+    <img src="../src/assets/Bell.svg" />
     </div>
     </div>
   )
