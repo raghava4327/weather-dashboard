9ca55c0a91360fbcf46e0d0b95fd5a62289fd004
Sai Raghava Ithireddy <131619104+raghava4327@users.noreply.github.com>
2024-02-06 12:44:04 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,12 +4,13 @@ export default function Navbar(){
   return(
     <div className="navbar">
     <h1>Weather Dashboard</h1>
-    <div className="navbar-icons">
+    
     <input type="text" placeholder="Enter location" id="" />
+    <div>
     <button className="button-30">search</button>
     <button className="button-30">Get current </button>
     </div>
-    
     </div>
+
   )
 }
