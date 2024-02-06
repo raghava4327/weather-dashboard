dd0e691c92c3df6e816fab89ac287c6032be5dc6
Sai Raghava Ithireddy <131619104+raghava4327@users.noreply.github.com>
2024-02-06 10:52:52 +0530

    updating navbar and rainfall

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react"
+import bell from "../src/assets/Bell.svg"
 export default function Navbar(){
   return(
     <div className="navbar">
     <h1>Weather Dashboard</h1>
     <input type="text" placeholder="Enter location or pincode"/>
     <div className="navbar-icons">
-    <img src="../src/assets/Bell.svg" />
+    <img src={bell}/>
     </div>
     </div>
   )
--- components/Rainfall.jsx
+++ components/Rainfall.jsx
@@ -1,21 +1,25 @@
 import React from "react"
 import {Theme} from "./WeatherReports"
+import rain from "../src/assets/32/Color=On-8.svg"
+import snow from "../src/assets/32/Color=On-22.svg"
+import wind from "../src/assets/32/Color=On-23.svg"
+
 export default function Rainfall(){
   const weatherVal=React.useContext(Theme)
   return(
     
     <div className="some-weather-conditions">
-    <img src="../src/assets/32/Color=On-8.svg" />
+    <img src= {rain}/>
       <div>
     <h2>Rainfall</h2>
     {weatherVal.rain?weatherVal.rain:<p>no rain today</p>}
     </div>
-    <img src="../src/assets/32/Color=On-22.svg" />
+    <img src= {snow}/>
       <div>
     <h2>Snow</h2>
     {weatherVal.rain?weatherVal.rain:<p>no snow today</p>}
     </div>
-    <img src="../src/assets/32/Color=On-23.svg" />
+    <img src={wind} />
       <div>
     <h2>Wind</h2>
     <p>wind speed : {weatherVal.wind.speed} meter/sec</p>
