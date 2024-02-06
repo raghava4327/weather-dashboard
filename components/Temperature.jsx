3156e0b41c3f3a3a0518f87bd97a46a545c32255
Sai Raghava Ithireddy <131619104+raghava4327@users.noreply.github.com>
2024-02-06 10:23:21 +0530

    Update Temperature.jsx

--- components/Temperature.jsx
+++ components/Temperature.jsx
@@ -4,13 +4,13 @@ export default function Temperature(){
   const weatherVal=React.useContext(Theme)
   return(
     <div className="temperature">
-      <img src="../src/assets/hot.gif" alt="" />
+      <img src="./src/assets/hot.gif" alt="" />
     
     <div>
     <h3>Today's max Temperature</h3>
     <p>{weatherVal.main.temp_max}°C</p>
     </div>
-    <img src="../src/assets/cold.png" alt="" />
+    <img src="./src/assets/cold.gif" alt="" />
     <div>
     <h3>Today's min Temperature</h3>
     <p>{weatherVal.main.temp_min}°C</p>
