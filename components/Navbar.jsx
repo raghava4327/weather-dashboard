4edfbab26102ef38f07c4d69a0cd3031c9f629d7
Sai Raghava Ithireddy <131619104+raghava4327@users.noreply.github.com>
2024-02-06 11:31:09 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,9 +4,8 @@ export default function Navbar(){
   return(
     <div className="navbar">
     <h1>Weather Dashboard</h1>
-    <input type="text" placeholder="Enter location or pincode"/>
     <div className="navbar-icons">
-    <img src={bell}/>
+    <img src={bell} alt="notifications"/>
     </div>
     </div>
   )
