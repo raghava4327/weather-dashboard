b07e24edd88ce0472c2cc8235430dd95c07ef83c
Sai Raghava Ithireddy <131619104+raghava4327@users.noreply.github.com>
2024-02-06 10:48:35 +0530

    updating temperature and weather details

--- components/Temperature.jsx
+++ components/Temperature.jsx
@@ -1,17 +1,18 @@
 import React from "react"
 import {Theme} from "./WeatherReports"
-import hot from "../src/assets/cold.gif"
+import cold from "../src/assets/cold.gif"
+import hot from "../src/assets/hot.gif"
 export default function Temperature(){
   const weatherVal=React.useContext(Theme)
   return(
     <div className="temperature">
-      <img src="../src/assets/hot.gif" alt="warm gif" />
+      <img src={hot} alt="warm gif" />
     
     <div>
     <h3>Today's max Temperature</h3>
     <p>{weatherVal.main.temp_max}°C</p>
     </div>
-    <img src={hot} alt="cold gif" />
+    <img src={cold} alt="cold gif" />
     <div>
     <h3>Today's min Temperature</h3>
     <p>{weatherVal.main.temp_min}°C</p>
--- components/WeatherDetails.jsx
+++ components/WeatherDetails.jsx
@@ -1,15 +1,19 @@
 import React from "react"
 import WeatherDetailsCards from "./WeatherDetailsCards"
+import sunrise from "../src/assets/32/Color=On-2.svg"
+import sunset from "../src/assets/32/Color=On-3.svg"
+import humidity from "../src/assets/32/Color=On-7.svg"
+import pressure from "../src/assets/32/Color=On-6.svg"
 import {Theme} from "./WeatherReports"
 export default function WeatherDetails(){
   const weatherVal=React.useContext(Theme)
   return(
     <div className="weather-details">
-      <WeatherDetailsCards image="../src/assets/32/Color=On-2.svg" details={new Date(weatherVal.sys.sunrise*1000+(weatherVal.timezone*1000)).toUTCString()}>Sunrise</WeatherDetailsCards>
-      <WeatherDetailsCards image="../src/assets/32/Color=On-3.svg" details={new Date(weatherVal.sys.sunset*1000+(weatherVal.timezone*1000)).toUTCString()}>Sunset</WeatherDetailsCards>
+      <WeatherDetailsCards image={sunrise} details={new Date(weatherVal.sys.sunrise*1000+(weatherVal.timezone*1000)).toUTCString()}>Sunrise</WeatherDetailsCards>
+      <WeatherDetailsCards image={sunset} details={new Date(weatherVal.sys.sunset*1000+(weatherVal.timezone*1000)).toUTCString()}>Sunset</WeatherDetailsCards>
       
-      <WeatherDetailsCards image="../src/assets/32/Color=On-7.svg" details={`${weatherVal.main.humidity}%`}>Humidity</WeatherDetailsCards>
-      <WeatherDetailsCards image="../src/assets/32/Color=On-6.svg" details={`${weatherVal.main.pressure} hPa`}>Pressure</WeatherDetailsCards>
+      <WeatherDetailsCards image={humidity} details={`${weatherVal.main.humidity}%`}>Humidity</WeatherDetailsCards>
+      <WeatherDetailsCards image={pressure} details={`${weatherVal.main.pressure} hPa`}>Pressure</WeatherDetailsCards>
       </div>
   )
 }
