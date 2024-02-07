174caaaf4cea041658a34fc6b852fc5032acfbec
Sai Raghava Ithireddy <131619104+raghava4327@users.noreply.github.com>
2024-02-07 20:27:43 +0530

    Update WeatherCard.jsx

--- components/WeatherCard.jsx
+++ components/WeatherCard.jsx
@@ -3,12 +3,21 @@ import {Theme} from "./WeatherReports"
 import CountryList from "./CountryList"
 export default function WeatherCard(){
   const weatherVal=React.useContext(Theme)
+  const [time,setTime]=React.useState(new Date().getTime())
+  React.useEffect(()=>{
+   const timer= setTimeout(()=>{
+      setTime(new Date().getTime())
+        },1000)
+    return ()=> clearTimeout(timer)
+  },[time])
+  
+  
   return(
     <div className="weather-card">
           <h1 className="today-temp">{Math.floor(weatherVal.main.temp)}°C</h1>
            <img className="weather-img" src={`https://openweathermap.org/img/wn/${weatherVal.weather[0].icon}@2x.png`}/>
           <p className="location">{weatherVal.name}<br/><span><CountryList con={weatherVal.sys.country}/></span></p>
-          <p className="time">{new Date(weatherVal.dt*1000+(weatherVal.timezone*1000)).toUTCString()} <br /></p>
+          <p className="time">{new Date(time).toLocaleString()} <br /></p>
       </div>
   )
 }
